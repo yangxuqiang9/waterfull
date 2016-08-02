dda280e0186a430aab32dcd7591fbfed36e92822
yxq <932477876@qq.com>
2016-08-02 14:45:08 +0800

    瀑布流第一页完成

--- app/src/main/java/com/example/a328789/waterfull/view/WaterFullView.java
+++ app/src/main/java/com/example/a328789/waterfull/view/WaterFullView.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.example.a328789.waterfull.R;
+import com.example.a328789.waterfull.bean.ImageBean;
 import com.example.a328789.waterfull.service.ImageLoad;
 
 import java.util.ArrayList;
@@ -26,8 +28,13 @@ public class WaterFullView extends ScrollView{
     private boolean isOneLoad;
     private int pager;
     private int pagerSize=20;
-    private List<String> imageUrls;
+    private String[] imageUrls;
     private ImageLoad imageLoad;
+    private List<ImageView> iamgeViewList=new ArrayList<>();
+    private int fristColumnHeight;
+    private int secondColumnHerght;
+    private LinearLayout fristColumn;
+    private LinearLayout secondColumn;
 
     public WaterFullView(Context context) {
         super(context);
@@ -51,7 +58,7 @@ public class WaterFullView extends ScrollView{
     private void init(Context context) {
         imageLoad = ImageLoad.getInstace(context);
         taskSet = new HashSet<>();
-        imageUrls = new ArrayList<String>();
+        imageUrls = ImageBean.images;
     }
 
     @Override
@@ -59,8 +66,8 @@ public class WaterFullView extends ScrollView{
         super.onLayout(changed, l, t, r, b);
         if(!isOneLoad){
             isOneLoad=true;
-            LinearLayout fristColumn = (LinearLayout) findViewById(R.id.frist_column);
-            LinearLayout secondColumn = (LinearLayout) findViewById(R.id.second_column);
+            fristColumn = (LinearLayout) findViewById(R.id.frist_column);
+            secondColumn = (LinearLayout) findViewById(R.id.second_column);
             //获取单列宽
             columnWidth = fristColumn.getWidth();
 
@@ -75,15 +82,15 @@ public class WaterFullView extends ScrollView{
     private void loadImage() {
         int startIndex=pager*pagerSize;
         int endIndex=(pager+1)*pagerSize-1;
-        if(imageUrls.size()>=startIndex){
-            if(endIndex>imageUrls.size()){
-                endIndex=imageUrls.size();
+        if(imageUrls.length>=startIndex){
+            if(endIndex>imageUrls.length){
+                endIndex=imageUrls.length;
             }
             for (int i=startIndex;i<=endIndex;i++){
                 //开启异步线程加载图片
                 LoadImageTask loadImageTask = new LoadImageTask();
                 taskSet.add(loadImageTask);
-                loadImageTask.execute(imageUrls.get(i));
+                loadImageTask.execute(imageUrls[i]);
             }
             pager++;
         }else {
@@ -109,7 +116,36 @@ public class WaterFullView extends ScrollView{
         protected void onPostExecute(Bitmap bitmap) {
             super.onPostExecute(bitmap);
             if(bitmap!=null){
+                double scale = bitmap.getWidth() / (columnWidth * 1.0);
+                int scaleHeight= (int) (bitmap.getHeight()/scale);
 
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(columnWidth, scaleHeight);
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(layoutParams);
+                imageView.setImageBitmap(bitmap);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imageView.setPadding(5,5,5,5);
+//                imageView.setTag(0,url);
+                //放倒最短的一列中
+                findShortColumn(scaleHeight,imageView).addView(imageView);
+                iamgeViewList.add(imageView);
+            }
+            taskSet.remove(this);
+        }
+
+        /**
+         * 找到最短的那一列
+         * @param scaleHeight
+         * @param imageView
+         * @return
+         */
+        private LinearLayout findShortColumn(int scaleHeight, ImageView imageView) {
+            if(fristColumnHeight<=secondColumnHerght){
+                fristColumnHeight+=scaleHeight;
+                return fristColumn;
+            }else {
+                secondColumnHerght+=scaleHeight;
+                return secondColumn;
             }
         }
     }
